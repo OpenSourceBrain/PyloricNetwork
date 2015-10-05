28db4a3e0bdec423bd973b95c5e2cfc72c578e09
Padraig Gleeson <p.gleeson@gmail.com>
2015-10-05 15:19:59 +0100

    Restructuring...

--- NeuroML2/GenerateFig2.py
+++ NeuroML2/GenerateFig2.py
@@ -2,30 +2,39 @@
 from neuroml import *
 
 from pyneuroml import pynml
-
+from matplotlib import pyplot as plt
 import sys
 
-def generate_panel(ref):
-    example_lems_file = 'LEMS_Fig2.xml'
+lems_files = {}
+lems_files['a'] = 'LEMS_Fig2a.xml'
+lems_files['b'] = 'LEMS_Fig2b.xml'
 
-    results = pynml.run_lems_with_jneuroml_neuron(example_lems_file, nogui=True, load_saved_data=True)
+def generate_panel(ref):
+    example_lems_file = lems_files[ref]
+
+    results = pynml.run_lems_with_jneuroml(example_lems_file, nogui=True, load_saved_data=True)
     
     if not '-nogui' in sys.argv:
+        print("Plotting results of %s"%ref)
+        
+        plots = len(results.keys())-1
+        f, a = plt.subplots(plots, sharex=True, sharey=True)
 
-        from matplotlib import pyplot as plt
-
+        count = 0
         for key in results.keys():
 
-            plt.xlabel('Time (s)')
-            plt.ylabel('Membrane potential (V)')
-            plt.grid('on')
+            #plt.xlabel('Time (s)')
+            #plt.ylabel('Membrane potential (V)')
+            #plt.grid('on')
 
             if key != 't':
-                plt.plot(results['t'],results[key], label=""+key)
+                a[count].plot(results['t'],results[key], label=""+key)
+                count+=1
 
-
-    plt.show()
     
 if __name__ == "__main__":
     
     generate_panel('a')
+    generate_panel('b')
+
+    plt.show()
